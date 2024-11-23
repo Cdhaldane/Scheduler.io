35c598b2d6f288439463e2d9fe31b2256ca442bb
Bella-S-W <71353604+Bella-S-W@users.noreply.github.com>
2024-11-23 11:25:01 -0500

    Update AnimatedDiv.jsx commented code

--- client/src/Components/AnimatedDiv/AnimatedDiv.jsx
+++ client/src/Components/AnimatedDiv/AnimatedDiv.jsx
@@ -2,6 +2,20 @@ import React, { useState, useEffect, useRef } from "react";
 import { useDidMountEffect } from "../../Utils";
 import "./AnimatedDiv.css";
 
+/**
+ * AnimatedDiv Component
+ *
+ * Purpose:
+ * - A wrapper `div` component that supports customizable CSS animations for entry and exit transitions.
+ * - The animation changes dynamically based on the `enterAnimation` and `exitAnimation` props.
+ *
+ * Notes:
+ * - Ensure `useDidMountEffect` is correctly implemented to trigger effects only after the initial render.
+ * - Define animations in the `AnimatedDiv.css` file to match the `enterAnimation` and `exitAnimation` prop values.
+ */
+
+
+
 const AnimatedDiv = ({
   className = "",
   enterAnimation,
